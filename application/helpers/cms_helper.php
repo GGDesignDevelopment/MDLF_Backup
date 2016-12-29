176f4a1b42897d8d7e8fe712f83b35db846dd128
Juan Ageitos <andresageitos@gmail.com>
2016-12-29 02:30:22 -0300

    Revert "Primer Commit"
    This reverts commit 25d5380eedeb2376faae3e852802f8f5e8d36f5a.

--- application/helpers/cms_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function btn_edit($uri) {
-	return anchor($uri, '<i class="glyphicon glyphicon-edit"></i>');
-}
-
-function btn_delete($uri) {
-	return anchor($uri, '<i class="glyphicon glyphicon-remove"></i>', 
-		array('onclick' => "return confirm('¿Desea eliminar este registro?');")); 
-}
--- application/views/frontend/_layout_modal.php
+++
@@ -1,3 +0,0 @@
-<?php $this->load->view('frontend/components/page_head');?>
-<?php $this->load->view($subview);?>
-<?php $this->load->view('frontend/components/page_footer');?>
